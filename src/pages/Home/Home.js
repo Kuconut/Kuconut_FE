a674252adb7f4d4574e6d7ecde7b716413f1f838
hyeonseo1018 <agileahn0114@gmail.com>
2024-06-27 18:20:06 +0900

    recent

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../../App.css';
+import './Home.css';
 
 const Home = () => {
   const navigate = useNavigate();
@@ -21,12 +22,17 @@ const Home = () => {
     navigate("/Study");
   }
 
+
   return (
+
     <div className='Home'>
-      <button onClick={goToPlay}className='button'>Play</button>
-      <button onClick={goToEat}className='button'>Eat</button>
-      <button onClick={goToExtra}className='button'>Extra</button>
-      <button onClick={goToStudy}className='button'>Study</button>
+      <div className = 'buttons'>
+        <button onClick={goToPlay}className='button'>Play</button>
+        <button onClick={goToEat}className='button'>Eat</button>
+        <button onClick={goToExtra}className='button'>Extra</button>
+        <button onClick={goToStudy}className='button'>Study</button>
+      </div>
+      
     </div>
   );
 }
